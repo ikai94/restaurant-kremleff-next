025c5abf317d37fc38be60dcd4a841358852ca6c
theantony1994 <theantony1994@yandex.ru>
2026-07-20 23:40:31 +0300

    add out

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from 'next';
 
+const repoName = 'restaurant-kremleff-next';
+const isGitHubActions = process.env.GITHUB_ACTIONS === 'true';
+
 const nextConfig: NextConfig = {
     reactStrictMode: true,
+
+    output: 'export',
+    trailingSlash: true,
+
+    basePath: isGitHubActions ? `/${repoName}` : '',
+    assetPrefix: isGitHubActions ? `/${repoName}/` : '',
+
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
